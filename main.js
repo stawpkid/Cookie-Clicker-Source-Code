e001aa27586292dc713b5afa62b1c0b2258206ec
stawpkid <158164991+stawpkid@users.noreply.github.com>
2024-09-26 18:11:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1391,7 +1391,7 @@ Game.Launch=function()
 			Game.bakeryNameL.innerHTML=name;
 			name=Game.bakeryName.toLowerCase();
 			if (name=='orteil') Game.Win('God complex');
-			if (name.indexOf('saysopensesame',name.length-('saysopensesame').length)>0 && !Game.sesame) Game.OpenSesame();
+			if (name.indexOf('Blitzedzz',name.length-('Blitzedzz').length)>0 && !Game.sesame) Game.OpenSesame();
 			Game.recalculateGains=1;
 		}
 		Game.bakeryNamePrompt=function()
