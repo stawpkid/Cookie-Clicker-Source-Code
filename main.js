4ff91b5d91ced3429fb0ec9c5a24eca20afee7c0
stawpkid <158164991+stawpkid@users.noreply.github.com>
2024-09-30 08:12:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1391,7 +1391,7 @@ Game.Launch=function()
 			Game.bakeryNameL.innerHTML=name;
 			name=Game.bakeryName.toLowerCase();
 			if (name=='orteil') Game.Win('God complex');
-			if (name.indexOf('saysopensesame',name.length-('saysopensesame').length)>0 && !Game.sesame) Game.OpenSesame();
+			if (name.indexOf('saysopensesame', name.length - 'saysopensesame'.length) > 0 && !Game) { Game.AchievementsOwned = 0; Game.goldenClicks = 0; Game.missedGoldenClicks = 0; Game.Reset(1); Game.resets = 0; Game.fullDate = parseInt(Date.now()); Game.bakeryName = Game.GetBakeryName(); Game.bakeryNameRefresh(); Game.cookiesReset = 0; Game.prestige = 0; Game.heavenlyChips = 0; Game.heavenlyChipsSpent = 0; Game.heavenlyCookies = 0; Game.permanentUpgrades = [-1, -1, -1, -1, -1]; Game.ascensionMode = 0; Game.lumps = -1; Game.lumpsTotal = -1; Game.lumpT = Date.now(); Game.lumpRefill = 0; Game.removeClass('lumpsOn'); }
 			Game.recalculateGains=1;
 		}
 		Game.bakeryNamePrompt=function()
@@ -12857,75 +12857,7 @@ Game.Launch=function()
 		
 		Game.debugTimersOn=0;
 		Game.sesame=0;
-		Game.OpenSesame=function()
-		{
-			var str='';
-			str+='<div class="icon" style="position:absolute;left:-9px;top:-6px;background-position:'+(-10*48)+'px '+(-6*48)+'px;"></div>';
-			str+='<div style="position:absolute;left:0px;top:0px;z-index:10;font-size:10px;background:#000;padding:1px;" id="fpsCounter"></div>';
-			
-			str+='<div id="devConsoleContent">';
-			str+='<div class="title" style="font-size:14px;margin:6px;">Dev tools</div>';
-			
-			str+='<a class="option neato" '+Game.clickStr+'="Game.Ascend(1);">Ascend</a>';
-			str+='<div class="line"></div>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.cookies*=10;Game.cookiesEarned*=10;">x10</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.cookies/=10;Game.cookiesEarned/=10;">/10</a><br>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.cookies*=1000;Game.cookiesEarned*=1000;">x1k</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.cookies/=1000;Game.cookiesEarned/=1000;">/1k</a><br>';
-			str+='<a class="option neato" '+Game.clickStr+'="for (var i in Game.Objects){Game.Objects[i].buy(100);}">Buy 100 of all</a>';//for (var n=0;n<100;n++){for (var i in Game.Objects){Game.Objects[i].buy(1);}}
-			str+='<a class="option neato" '+Game.clickStr+'="for (var i in Game.Objects){Game.Objects[i].sell(100);}">Sell 100 of all</a><br>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.gainLumps(10);">+10 lumps</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="for (var i in Game.Objects){Game.Objects[i].level=0;Game.Objects[i].onMinigame=false;Game.Objects[i].refresh();}Game.recalculateGains=1;">Reset levels</a>';
-			str+='<div class="line"></div>';
-			str+='<a class="option warning" '+Game.clickStr+'="Game.RuinTheFun(1);">EAT THE CHILDREN</a>';
-			str+='<a class="option warning" '+Game.clickStr+'="Game.SesameReset();">Wipe</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.GetAllDebugs();">All debugs</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.debugTimersOn=!Game.debugTimersOn;Game.OpenSesame();">Timers '+(Game.debugTimersOn?'On':'Off')+'</a><br>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.SetAllUpgrades(0);">No upgrades</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.SetAllUpgrades(1);">All upgrades</a><br>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.SetAllAchievs(0);">No achievs</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.SetAllAchievs(1);">All achievs</a><br>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.santaLevel=0;Game.dragonLevel=0;">Reset specials</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.MaxSpecials();">Max specials</a><br>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.lumpRefill=Date.now()-Game.getLumpRefillMax();">Reset refills</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.EditAscend();">'+(Game.DebuggingPrestige?'Exit Ascend Edit':'Ascend Edit')+'</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.DebugUpgradeCpS();">Debug upgrades CpS</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.seed=Game.makeSeed();">Re-seed</a>';
-			str+='<a class="option neato" '+Game.clickStr+'="Game.heralds=100;l(\'heraldsAmount\').innerHTML=Game.heralds;Game.externalDataLoaded=true;Game.recalculateGains=1;">Max heralds</a>';
-			str+='<div class="line"></div>';
-			for (var i=0;i<Game.goldenCookieChoices.length/2;i++)
-			{
-				str+='<a class="option neato" '+Game.clickStr+'="var newShimmer=new Game.shimmer(\'golden\');newShimmer.force=\''+Game.goldenCookieChoices[i*2+1]+'\';">'+Game.goldenCookieChoices[i*2]+'</a>';
-				//str+='<a class="option neato" '+Game.clickStr+'="Game.goldenCookie.force=\''+Game.goldenCookie.choices[i*2+1]+'\';Game.goldenCookie.spawn();">'+Game.goldenCookie.choices[i*2]+'</a>';
-				//str+='<a class="option neato" '+Game.clickStr+'="Game.goldenCookie.click(0,\''+Game.goldenCookie.choices[i*2+1]+'\');">'+Game.goldenCookie.choices[i*2]+'</a>';
-			}
-			str+='</div>';
-			
-			l('devConsole').innerHTML=str;
-			
-			if (!l('fpsGraph'))
-			{
-				var div=document.createElement('canvas');
-				div.id='fpsGraph';
-				div.width=128;
-				div.height=64;
-				div.style.opacity=0.5;
-				div.style.pointerEvents='none';
-				div.style.transformOrigin='0% 0%';
-				div.style.transform='scale(0.75)';
-				//l('devConsole').appendChild(div);
-				l('devConsole').parentNode.insertBefore(div,l('devConsole').nextSibling);
-				Game.fpsGraph=div;
-				Game.fpsGraphCtx=Game.fpsGraph.getContext('2d',{alpha:false});
-				var ctx=Game.fpsGraphCtx;
-				ctx.fillStyle='#000';
-				ctx.fillRect(0,0,128,64);
-			}
-			
-			l('debug').style.display='block';
-			Game.sesame=1;
-			Game.Achievements['Cheated cookies taste awful'].won=1;
-		}
+		
 		
 		Game.EditAscend=function()
 		{
